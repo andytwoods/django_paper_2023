20a0c5d88090ccaa0474c9395f74c0ec5555511f
andytwoods <andytwoods@gmail.com>
2023-10-21 12:53:14 +0100

    gitignore added

--- study/urls.py
+++ study/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>/', views.study),
-    path('signup/', views.signup, name='signup'),
+    path('<int:study_id>/', views.study),
+    path('<int:study_id>/signup/',
+         views.signup, name='signup'),
 ]
--- study/views.py
+++ study/views.py
@@ -1,16 +1,32 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Study
 
 
-def study(request, id: int):
+def study(request, study_id: int):
     try:
-        my_study = Study.objects.get(id=id)
+        my_study = Study.objects.get(id=study_id)
         context = {"study": my_study}
         return render(request, "study/study.html", context=context)
     except Study.DoesNotExist:
-        return HttpResponse(f'study {id} does not exist!')
+        return HttpResponse(f'study {study_id} does not exist!')
+
+# def study(request, study_id: int):
+#     my_study = Study.objects.get(id=study_id)
+#     context = {"study": my_study}
+#     return render(request, "study/study.html", context=context)
 
 
-def signup(request):
+# def study(request, study_id: int):
+#     if not request.user.is_authenticated:
+#         return redirect('home')
+#
+#
+# @login_required()
+# def study(request, study_id: int):
+#     ...
+
+
+def signup(request, study_id: int):
     return HttpResponse(f'Not implemented yet!')
